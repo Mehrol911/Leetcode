919fb377a43485ff4058f55e79c2b28159c7e58a
Mehrol Bazarov <80707348+Mehrol911@users.noreply.github.com>
2022-07-13 19:15:08 +0500

    Update 1890. The Latest Login in 2020.sql

--- 1890. The Latest Login in 2020.sql
+++ 1890. The Latest Login in 2020.sql
@@ -3,3 +3,6 @@ select user_id, max(time_stamp) as last_stamp
 from logins
 where time_stamp between '2020-01-01 00:00:00' and '2021-01-01 00:00:00'
 group by user_id;
+
+#second solution
+select user_id,max( time_stamp ) as last_stamp from logins where left(time_stamp,4)="2020" group by user_id
